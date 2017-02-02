ad2aa7ad1c17a18743969f5b6c20b873f80b2985
KushnerEvgen <kushner.jenia@gmail.com>
2017-02-02 19:26:40 +0300

    Day 1custom menu

--- app/src/main/java/com/example/jenek_pc/myapp/MainActivity.java
+++ app/src/main/java/com/example/jenek_pc/myapp/MainActivity.java
@@ -2,11 +2,24 @@ package com.example.jenek_pc.myapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] names = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+                "Костя", "Игорь", "Анна", "Денис", "Андрей", "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+                "Костя", "Игорь", "Анна", "Денис", "Андрей" };
+
+        ListView lvMain = (ListView) findViewById(R.id.lvMain);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, names);
+
+        lvMain.setAdapter(adapter);
     }
 }
